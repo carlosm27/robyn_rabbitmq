7a4ddb3377bab53bab261c0077e4f6a0365d71e7
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-08-14 14:10:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,27 @@ from robyn.robyn import Response
 import asyncio
 import aio_pika
 
-import ast
 app = Robyn(__file__)
 
 
 @app.get("/")
 async def hello():
+    body = "Hello World!"
+    await sender(body)
     return Response(status_code=status_codes.HTTP_200_OK, headers={}, body="Hello, World!")
 
+async def sender(body: str):
+    loop = asyncio.get_event_loop()
+    connection = await aio_pika.connect("amqp://guest:guest@localhost/", loop = loop)
+    channel =  await connection.channel()
+    
 
+
+    await channel.default_exchange.publish(aio_pika.Message(body=body.encode()), routing_key='hello')
+    print(" [x] Sent 'Hello'")
+    await connection.close()
+
+"""
 async def startup():
     loop = asyncio.get_event_loop()
     connection = await aio_pika.connect("amqp://guest:guest@localhost/", loop = loop)
@@ -27,5 +39,6 @@ async def startup():
     await queue.consume(callback)    
 
 app.startup_handler(startup)
+"""
 
 app.start(port=8000, url="0.0.0.0")
